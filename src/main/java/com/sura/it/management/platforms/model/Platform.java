0fb43f9546bccc258d623962fe0220008c18c781
carlcafl <ccarmona@gmail.com>
2015-04-28 16:12:45 -0500

    Model

---
+++ src/main/java/com/sura/it/management/platforms/model/Platform.java
@@ -0,0 +1,27 @@
+package com.sura.it.management.platforms.model;
+
+public class Platform {
+	
+	private long id;
+	private String shortName;
+	private String name;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getShortName() {
+		return shortName;
+	}
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
