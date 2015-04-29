68900e4f4cc4d6d734ff93f0d048e027a1d81e95
carlcafl <ccarmona@gmail.com>
2015-04-29 10:42:06 -0500

    Cambios de nombre de proyecto

--- src/main/java/com/sura/it/management/platforms/services/RESTPlatforms.java
+++ src/main/java/com/sura/it/management/platforms/services/RESTPlatforms.java
@@ -6,27 +6,28 @@ import java.util.List;
 //import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import com.sura.it.management.platforms.model.Platform;
 
-@Path("/platforms")
+
 public class RESTPlatforms {
 
 	
 	@GET
+	@Path("/platforms/{id}")
 	//@Consumes( "application/json" )
 	@Produces( "application/json" ) 
-	public List<Platform> list() {
-		System.out.println(this.getClass().getName() + "--> Entró");
+	public List<Platform> getPlatforms(@PathParam("id") String id) {
 		List<Platform> list = new ArrayList<Platform>();
-		
-		Platform p = new Platform();
-		p.setId(1);
-		p.setName("Core de Seguros");
-		p.setShortName("CORE_SEG");
-		
-		list.add(p);
+		if (id==null || "1".equals(id)) {
+			Platform p = new Platform();
+			p.setId(1);
+			p.setName("Core de Seguros");
+			p.setShortName("CORE_SEG");				
+			list.add(p);
+		}
 		
 		return list;
 	}
