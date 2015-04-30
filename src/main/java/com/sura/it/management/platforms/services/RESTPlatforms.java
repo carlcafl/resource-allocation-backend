9f7358769ef032a94948454e27eb995865ce81de
carlcafl <ccarmona@gmail.com>
2015-04-30 10:18:07 -0500

    Adición de header para los servicios REST

--- src/main/java/com/sura/it/management/platforms/services/RESTPlatforms.java
+++ src/main/java/com/sura/it/management/platforms/services/RESTPlatforms.java
@@ -5,11 +5,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
 //import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 
 import com.sura.it.management.platforms.dataAccess.PlatformsDataAccess;
 import com.sura.it.management.platforms.model.Platform;
@@ -17,7 +19,9 @@ import com.sura.it.management.platforms.model.Platform;
 @Path("/platforms")
 public class RESTPlatforms {
 
-	
+	@Context
+	private HttpServletResponse response;
+	  
 	@GET
 	@Path("/")
 	//@Consumes( "application/json" )
@@ -26,7 +30,7 @@ public class RESTPlatforms {
 		List<Platform> list = new ArrayList<Platform>();
 
 		list = PlatformsDataAccess.listPlatforms();
-		
+		processResponse();
 		return list;
 	}
 
@@ -35,6 +39,11 @@ public class RESTPlatforms {
 	//@Consumes( "application/json" )
 	@Produces( "application/json" ) 
 	public Platform getPlatform(@PathParam("id") int id) throws URISyntaxException, SQLException {
+		processResponse();
 		return PlatformsDataAccess.getPlatformById(id);
+	}
+	
+	private void processResponse() {
+		response.addHeader("Access-Control-Allow-Origin", "*");
 	}
 }
