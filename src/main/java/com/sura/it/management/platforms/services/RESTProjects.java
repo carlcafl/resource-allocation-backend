b8a21db1d945c8590bf3dd299b94733cbf4e32de
carlcafl <ccarmona@gmail.com>
2015-05-29 13:52:59 -0500

    Cambio de GET a POST

--- src/main/java/com/sura/it/management/platforms/services/RESTProjects.java
+++ src/main/java/com/sura/it/management/platforms/services/RESTProjects.java
@@ -8,6 +8,7 @@ import java.util.List;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -75,7 +76,7 @@ public class RESTProjects extends RESTService {
 		return projectTypes;
 	}
 	
-	@GET
+	@POST
 	@Path("/{id}/allocation")
 	@Consumes( MediaType.APPLICATION_JSON )
 	@Produces( MediaType.APPLICATION_JSON )
